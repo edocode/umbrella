f8f1084cee4780b12d11085ff7ef82cea4b83230
urakami.y <urakami.y@edocode.co.jp>
2023-11-10 18:26:13 +0900

    add get image backend

---
+++ src/functions/image.test.js
@@ -0,0 +1,56 @@
+import GetImage from "./image.js";
+import axios from "axios";
+
+const mockPost = jest.spyOn(axios, 'post');
+const mockGetEnv = jest.fn()
+global.Netlify = {
+    env: {
+        get: (key) => mockGetEnv(key)
+    }
+}
+const request = new Request("",{
+    method: "POST",
+    body: JSON.stringify({prompt: "string"}),
+})
+
+beforeEach(() => {
+    mockGetEnv.mockReturnValue('API_KEY')
+})
+
+it('should return 200 OK', async () => {
+    const res = await GetImage(request, undefined)
+    expect(res.status).toBe(200)
+})
+it('should call openai api',async()=>{
+    await GetImage(request, context);
+    expect(mockPost).toHaveBeenCalledWith('https://api.openai.com/v1/images/generations',
+    {
+        model: "dall-e-2",
+        prompt: "",
+        n: 1,
+        size: "256x256"
+    }, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer API_KEY"
+        }
+    })
+})
+it('should use API key from env variable', async () => {
+    mockGetEnv.mockReturnValue('different_api_key')
+    let spyAuthorization = ''
+    mockPost.mockImplementation(({}, {}, options) => {
+        spyAuthorization = options.headers.Authorization
+    })
+    await GetImage(request, context)
+    expect(mockGetEnv).toHaveBeenCalledWith('API_KEY')
+    expect(spyAuthorization).toBe("Bearer different_api_key")
+})
+
+it('should call openai api using  requested prompt ', async()=>{
+    await GetImage(request,context)
+    expect(mockPost).toHaveBeenCalledWith(expect.anything(),
+        expect.objectContaining({
+            prompt: "string",
+        }), expect.anything())
+})
