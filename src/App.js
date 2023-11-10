c02487fa2e60de09bd1f32cb86c102f5a60cd001
agarwal.s <agarwal.s@EDOPC-093.local>
2023-11-10 21:29:57 +0900

    Fix style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
+import logo from './logo.jpeg';
 import './App.css';
 import PromptPage from "./Components/PromptPage";
+import React from "react";
 
 function App() {
+    const [startGame, setStartGame] = React.useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Umbrella</h3>
-      </header>
-        <PromptPage topic={'umbrella'} disabled={false}/>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h3 className="Game-name">Umbrella</h3>
+            <div></div>
+
+        </header>
+
+        {!startGame && <div className="Start-button">{!startGame && <button className="button" onClick={() => setStartGame(true)}>START GAME!</button>}</div>}
+        {startGame && <PromptPage disabled={false}/>}
     </div>
   );
 }
--- src/Components/PromptPage.jsx
+++ src/Components/PromptPage.jsx
@@ -4,19 +4,76 @@ import axios from 'axios';
 import $ from 'jquery';
 import "./../style.css";
 
-const PromptPage = ({topic, disabled})  =>  {
+const PromptPage = ({disabled})  =>  {
+        const words = [
+        "apple",
+        "banana",
+        "carrot",
+        "dolphin",
+        "elephant",
+        "firetruck",
+        "guitar",
+        "helicopter",
+        "jigsaw",
+        "kangaroo",
+        "lighthouse",
+        "moon",
+        "nest",
+        "octopus",
+        "penguin",
+        "quilt",
+        "rainbow",
+        "sunflower",
+        "tiger",
+        "umbrella",
+        "violin",
+        "waterfall",
+        "xylophone",
+        "yarn",
+        "zebra",
+        "acorn",
+        "beach",
+        "castle",
+        "dragon",
+        "envelope",
+        "flamingo",
+        "globe",
+        "hiking",
+        "instrument",
+        "jellyfish",
+        "kiwi",
+        "lemon",
+        "mountain",
+        "notebook",
+        "owl",
+        "palm tree",
+        "raccoon",
+        "snail",
+        "tornado",
+        "unicorn",
+        "volcano",
+        "watermelon",
+        "yoga"
+    ]
+
+    const pickRandomWord = () => {
+        const item = words[Math.floor(Math.random()*words.length)]
+        setShowTopic(item)
+    }
+
     const generateImage = async (prompt) => {
 
         const words = prompt.split(" ");
         let flag = 0
 
-        words.map(word => word === topic? flag = 1 : null);
+        words.map(word => word === showTopic? flag = 1 : null);
         if(flag === 1) {
             setShowError(true)
-}
+        }
         if (flag === 0) {
             setShowError(false)
             setImageLoading(true)
+            setShowTimer(false)
 
             try {
                 const {data} = await axios.post(
@@ -26,6 +83,7 @@ const PromptPage = ({topic, disabled})  =>  {
                     },
                 );
 
+                setEnableSubmit(false)
                 setImageLoading(false)
                 setShowImage(data.imageUrl);
             } catch (error) {
@@ -42,31 +100,33 @@ const PromptPage = ({topic, disabled})  =>  {
     });
 
 
-    const [showTopic, setShowTopic] = React.useState(false);
+    const [showTopic, setShowTopic] = React.useState('');
     const [showImage, setShowImage] = React.useState('');
     const [showError, setShowError] = React.useState(false);
     const [showTimesUp, setShowTimesUp] = React.useState(false);
     const [imageLoading, setImageLoading] = React.useState(false);
-    let image;
+    const [enableSubmit, setEnableSubmit] = React.useState(true);
+    const [showTimer, setShowTimer] = React.useState(true);
 
     return (
         <>
             {!showTimesUp && (<div className="timeLeft">
-                <button disabled={disabled}  onClick={() => setShowTopic(true)}>Pick a topic</button>
-                {showTopic && <div>{topic}</div>}
-                <input id="promptText" name="promptText" placeholder="write your prompt"></input>
-                <input type={"button"} value={"Submit"} onClick={() => (image = generateImage(document.getElementById("promptText").value))} />
+                <div className="topicContainer">{showTopic === '' && <button className="randomWordButton" disabled={disabled}  onClick={() => pickRandomWord()}>Pick a topic</button>}
+                    {showTopic && <div className="topic">{showTopic}</div>}</div>
+
+                <input className="inputArea" id="promptText" name="promptText" placeholder="Type your prompt here"></input>
+                {enableSubmit && <input className="generatorImageButton" type={"button"} value={"Generate image"} onClick={() => (generateImage(document.getElementById("promptText").value))} />}
                 {imageLoading &&
                     (<><div>Generating image for you...</div>
                         <span className="loader"></span></>)}
 
 
-                <div><CountdownCircleTimer isPlaying
-                                           children
-                                           duration={60}
-                                           colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-                                           colorsTime={[7, 5, 2, 0]}>{({ remainingTime }) => "You can do it in " + remainingTime}</CountdownCircleTimer></div>
-                {showError && <div>Don't use the banned word</div>}
+                {showTimer && <div><CountdownCircleTimer isPlaying
+                                                            children
+                                                            duration={60}
+                                                            colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+                                                            colorsTime={[7, 5, 2, 0]}>{({ remainingTime }) => "You can do it in " + remainingTime}</CountdownCircleTimer></div>}
+                {showError && <div className="warning">Do not use the banned word!!</div>}
                 {showImage && <img alt="generated-image" src={showImage} width="256" height="256"/> }
             </div>)}
             {showTimesUp && (<div>Times Up!</div>)}
