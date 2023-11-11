ae58d0676669b1672b280536e9948864b0ee0dfc
Desmond Chin <chin.d@edocode.co.jp>
2023-11-11 11:22:32 +0900

    link creating game session to starting game

--- src/App.js
+++ src/App.js
@@ -5,21 +5,16 @@ import React from "react";
 import UserPage from "./Components/UserPage";
 
 function App() {
-    const [startGame, setStartGame] = React.useState(false);
   return (
     <div className="App">
         <header className="App-header">
                 <img className="App-logo" src={logo} alt="logo" />
-                <h3 className="Game-name" >Umbrella</h3>
+                <h3 className="Game-name" >Pic-kasa</h3>
             <div id='spacer'></div>
         </header>
         <container>
-
-            <UserPage/>
-            <div className="introText">Generate the best image without using the banned word
-               <br></br> and win the game ! </div>
-            {!startGame && <div className="Start-button">{!startGame && <button className="button" onClick={() => setStartGame(true)}>START GAME!</button>}</div>}
-            {startGame && <PromptPage disabled={false}/>}
+            <><UserPage/>
+            </>
         </container>
 
     </div>
--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -2,6 +2,7 @@ import {getDatabase, onValue, push, ref, set, get} from "firebase/database";
 import React, {useEffect, useState} from "react";
 import {initializeApp} from "firebase/app";
 import './UserPage.css';
+import PromptPage from './PromptPage'
 
 const firebaseConfig = {
     databaseURL: process.env.REACT_APP_DB_URL,
@@ -18,30 +19,41 @@ function UserPage(){
     const [step, setStep] = useState(0)
     const [isHost, setIsHost] = useState(false)
     const [sessionId, setSessionId] = useState('')
+    const [startGame, setStartGame] = React.useState(false);
+    const [showPromptPage, setShowPromptPage] =React.useState(false)
+
+    useEffect(() => {
+        setShowPromptPage(true)
+    }, [startGame])
 
     if (step === 0 ) {
         return (
-            <div>
-                <button onClick={() => {
-                    setStep(1)
-                    setIsHost(true)
-                }}>Create Game</button>
-                <button onClick={() => {
-                    setStep(1)
-                }}>Join Game</button>
-            </div>
+            <>
+                <div className="buttonContainer" >
+                    <button className="createButton"  onClick={() => {
+                        setStep(1)
+                        setIsHost(true)
+                    }}>Create Game</button>
+                    <button className="joinButton"  onClick={() => {
+                        setStep(1)
+                    }}>Join Game</button>
+                </div>
+
+                <div className="introText">Generate the best image without using the banned word
+                    <br></br> and win the game ! </div></>
+
         )
     }
 
     if (step === 1) {
         if (isHost) {
             return (
-                <div>
-                    <label>
+                <div className="hostContainer">
+                    <label className="nameHeader">
                         Name
-                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                        <input className="inputName" type="text" value={name} onChange={(e) => setName(e.target.value)} />
                     </label>
-                    <button onClick={async () => {
+                    <button  className="createButton" onClick={async () => {
                         const sessionsRef = ref(db, 'sessions');
                         const newSessionRef = push(sessionsRef);
                         await set(newSessionRef, {
@@ -50,6 +62,16 @@ function UserPage(){
                         const usersRef = ref(db, `sessions/${newSessionRef.key}/users`)
                         const userRef = push(usersRef)
                         await set(userRef, name)
+
+                        // listen to new changes to player list
+                        onValue(usersRef, (snapshot) => {
+                            var newUsers = [];
+                            snapshot.forEach((childSnapshot) => {
+                                newUsers.push(childSnapshot.val());
+                            })
+                            setUsers(newUsers);
+                        });
+
                         setSessionId(newSessionRef.key)
                         setStep(2)
                     }}>Create Game</button>
@@ -58,16 +80,18 @@ function UserPage(){
         } else {
             return (
                 <div>
-                    <label>
-                        Game Id
-                        <input type="text" value={sessionId} onChange={(e) => setSessionId(e.target.value)}/>
-                    </label>
-                    <label>
-                        Name
-                        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-                    </label>
+                    <div className="inputContainer">
+                        <label className="nameHeader">
+                            Game Id
+                            <input  className="inputGameid" type="text" value={sessionId} onChange={(e) => setSessionId(e.target.value)}/>
+                        </label>
+                        <label className="nameHeader">
+                            Name
+                            <input className="inputName"  type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+                        </label>
+                    </div>
 
-                    <button onClick={async () => {
+                    <button  className="joinButton" onClick={async () => {
                         const snapshot = await get(ref(db, `sessions/${sessionId}`))
                         if (!snapshot.exists()) {
                             alert('Game ID does not exist')
@@ -87,7 +111,7 @@ function UserPage(){
                         await set(userRef, name)
 
                         setStep(2)
-                    }}>Create Game
+                    }}>Join Game
                     </button>
                 </div>
             )
@@ -96,10 +120,23 @@ function UserPage(){
     }
 
     return(<>
-            <h1>Game ID: {sessionId}</h1>
-            <ol class="usersList">
-                {users && users.map(user => <li>{user}</li>)}
-            </ol>
+            {!startGame && <>
+                <div className="gameIdWrapper">
+                    <h1 className="sessionId">Game ID:
+                        <span className="userIdNumber"> {sessionId}</span>
+                        {isHost  && !startGame &&  <div className="shareText">Share the link to other players</div> }
+
+                    </h1>
+                </div>
+                <ol className="usersList">
+                    {users && users.map(user => <li>{user}</li>)}
+                   {isHost  && !startGame &&        <img className="hostLogo" src="logo.png" alt="logo512" />}
+                </ol>
+                {!isHost && <div className="nameHeader">Waiting for the host to start the game....</div>}
+            </>}
+
+            {isHost && !startGame && <div className="Start-button"><button className="button" onClick={() => setStartGame(true)}>START GAME!</button></div>}
+            {startGame && showPromptPage && <PromptPage disabled={false}/>}
         </>
     )
 }
