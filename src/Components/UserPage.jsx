1c5381b8598e57ab5429119a8e715fee681ae4fc
urakami.y <urakami.y@edocode.co.jp>
2023-11-11 09:18:37 +0900

    add 'create game' and 'join game'

--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -1,4 +1,4 @@
-import {getDatabase, onValue, push, ref, set} from "firebase/database";
+import {getDatabase, onValue, push, ref, set, get} from "firebase/database";
 import React, {useEffect, useState} from "react";
 import {initializeApp} from "firebase/app";
 import './UserPage.css';
@@ -15,29 +15,91 @@ function UserPage(){
     const [users, setUsers] = useState([])
     const [disabled, setDisabled] = React.useState(false);
 
-    useEffect(() => {
-        const usersRef = ref(db, 'users');
-        onValue(usersRef, (snapshot) => {
-            var newUsers = [];
-            snapshot.forEach((childSnapshot) => {
-                newUsers.push(childSnapshot.val());
-            })
-            setUsers(newUsers);
-        });
-    }, [])
+    const [step, setStep] = useState(0)
+    const [isHost, setIsHost] = useState(false)
+    const [sessionId, setSessionId] = useState('')
+
+    if (step === 0 ) {
+        return (
+            <div>
+                <button onClick={() => {
+                    setStep(1)
+                    setIsHost(true)
+                }}>Create Game</button>
+                <button onClick={() => {
+                    setStep(1)
+                }}>Join Game</button>
+            </div>
+        )
+    }
+
+    if (step === 1) {
+        if (isHost) {
+            return (
+                <div>
+                    <label>
+                        Name
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                    </label>
+                    <button onClick={async () => {
+                        const sessionsRef = ref(db, 'sessions');
+                        const newSessionRef = push(sessionsRef);
+                        await set(newSessionRef, {
+                            users: null
+                        });
+                        const usersRef = ref(db, `sessions/${newSessionRef.key}/users`)
+                        const userRef = push(usersRef)
+                        await set(userRef, name)
+                        setSessionId(newSessionRef.key)
+                        setStep(2)
+                    }}>Create Game</button>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <label>
+                        Game Id
+                        <input type="text" value={sessionId} onChange={(e) => setSessionId(e.target.value)}/>
+                    </label>
+                    <label>
+                        Name
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+                    </label>
+
+                    <button onClick={async () => {
+                        const snapshot = await get(ref(db, `sessions/${sessionId}`))
+                        if (!snapshot.exists()) {
+                            alert('Game ID does not exist')
+                            return
+                        }
+
+                        const usersRef = ref(db, `sessions/${sessionId}/users`)
+                        onValue(usersRef, (snapshot) => {
+                            var newUsers = [];
+                            snapshot.forEach((childSnapshot) => {
+                                newUsers.push(childSnapshot.val());
+                            })
+                            setUsers(newUsers);
+                        });
+
+                        const userRef = push(usersRef)
+                        await set(userRef, name)
+
+                        setStep(2)
+                    }}>Create Game
+                    </button>
+                </div>
+            )
+        }
+
+    }
+
     return(<>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-    <button onClick={() => {
-        const usersRef = ref(db, 'users');
-        const newPostRef = push(usersRef);
-        set(newPostRef, name);
-        setName('');
-        setDisabled(true)
-    }}
-    disabled={disabled}>Submit</button>
-    <ul class="usersList">
+            <h1>Game ID: {sessionId}</h1>
+    <ol class="usersList">
         {users && users.map(user => <li>{user}</li>)}
-    </ul>
+    </ol>
         </>
     )
 }
